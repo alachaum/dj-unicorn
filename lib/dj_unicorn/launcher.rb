5a85f3876daa4e1831c3da181bbf04b0b61e5de0
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2013-08-10 00:24:51 +1000

    extracted minimal launcher logic from unicorn

--- lib/dj_unicorn/launcher.rb
+++ lib/dj_unicorn/launcher.rb
@@ -1,3 +1,56 @@
+$stdout.sync = $stderr.sync = true
+$stdin.binmode
+$stdout.binmode
+$stderr.binmode
+
 module DJUnicorn::Launcher
   
+  # Creates the dj_unicorn master process
+  #   * umask is whatever was set by the parent process at startup
+  #     and can be set in config.ru and config_file, so making it
+  #     0000 and potentially exposing sensitive log data can be bad
+  #     policy.
+  #   * don't bother to chdir("/") here since unicorn is designed to
+  #     run inside APP_ROOT.  Unicorn will also re-chdir() to
+  #     the directory it was started in when being re-executed
+  #     to pickup code changes if the original deployment directory
+  #     is a symlink or otherwise got replaced.
+  def self.daemonize!(options)
+    $stdin.reopen("/dev/null")
+    
+    # We only start a new process group if we're not being reexecuted
+    unless ENV['DJ_UNICORN_REEXEC']
+      # grandparent - reads pipe, exits when master is ready
+      #  \_ parent  - exits immediately ASAP
+      #      \_ grandchild: dj_unicorn master - writes to pipe when ready
+      
+      # Create main pipe and get grandparent PID
+      rd, wr = IO.pipe
+      grandparent = $$
+      if fork
+        # Grandparent process - does not write
+        wr.close
+      else
+        # Parent Process
+        rd.close # parent/grandchild do not need to write
+        Process.setsid
+        exit if fork # create grandchild and die
+      end
+      
+      if grandparent == $$
+        # Wait for the dj_unicorn master process to write
+        # its PID on pipe
+        master_pid = (rd.readpartial(16) rescue nil).to_i
+        # Return failure code if process is root
+        
+        unless master_pid > 1
+          warn "master failed to start, check stderr log for details"
+          exit!(1)
+        end
+        exit 0
+      else # unicorn master process
+        options[:ready_pipe] = wr
+      end
+    end
+  end
 end
--- spec/lib/launcher_spec.rb
+++ spec/lib/launcher_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
-describe DJUnicorn::Launcher do
+# Process structure
+# grandparent - reads pipe, exits when master is ready
+#  \_ parent  - exits immediately ASAP
+#      \_ dj_unicorn master - writes to pipe when ready
 
+describe DJUnicorn::Launcher do
+  before(:each) do
+    @mod = DJUnicorn::Launcher
+  end
+  
+  describe "daemonize!" do
+    it "does not start a new process group if we are reexecuted" do
+      ENV['DJ_UNICORN_REEXEC'] = 'true'
+      @mod.should_not_receive(:fork)
+      @mod.daemonize!({})
+    end
+    
+    describe "grandparent process" do
+      before(:each) do
+        ENV['DJ_UNICORN_REEXEC'] = nil
+      end
+      
+      it "forks to create the parent process" do
+        stub_io_pipe_with_read
+        @mod.should_receive(:fork).and_return(123)
+        capture_sysexit { @mod.daemonize!({}) }
+      end
+      
+      it "waits for a child process to write on pipe before exiting" do
+        rd,wr = stub_io_pipe
+        
+        # For this test we want to avoid the grandparent
+        # to close the pipe
+        wr.stub(:close)
+        
+        th = Thread.new do
+          capture_sysexit { @mod.daemonize!({}) }
+        end
+        th.should be_alive
+        wr.syswrite(245.to_s)
+        th.join(5)
+        th.should_not be_alive
+      end
+    end
+    
+    describe "parent process" do
+      
+    end
+    
+    describe "dj_master process" do
+      
+    end
+  end
 end
